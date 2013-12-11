bd1df084a1e29c1fcc73a0b0e29287afc58049ae
Eliot Arntz <arntzel@gmail.com>
2013-12-11 17:36:10 +0100

    comments

---
+++ MatchedUp/CCConstants.m
@@ -0,0 +1,33 @@
+//
+//  CCConstants.m
+//  MatchedUp
+//
+//  Created by Eliot Arntz on 11/30/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import "CCConstants.h"
+
+@implementation CCConstants
+
+#pragma mark - User Class
+
+NSString *const kCCUserProfileKey               = @"profile";
+NSString *const kCCUserProfileNameKey           = @"name";
+NSString *const kCCUserProfileFirstNameKey      = @"firstName";
+NSString *const kCCUserProfileLocationKey       = @"location";
+NSString *const kCCUserProfileGenderKey         = @"gender";
+NSString *const kCCUserProfileBirthdayKey       = @"birthday";
+NSString *const kCCUserProfileInterestedInKey   = @"interestedIn";
+NSString *const kCCUserProfilePictureURL        = @"pictureURL";
+NSString *const kCCUserProfileAgeKey            = @"age";
+NSString *const kCCUserProfileRelationshipStatusKey = @"relationshipStatus";
+
+#pragma mark - Photo Class
+
+NSString *const kCCPhotoClassKey                = @"Photo";
+NSString *const kCCPhotoUserKey                 = @"user";
+NSString *const kCCPhotoPictureKey              = @"image";
+
+
+@end
---
+++ MatchedUp/CCHomeViewController.m
@@ -0,0 +1,95 @@
+//
+//  CCHomeViewController.m
+//  MatchedUp
+//
+//  Created by Eliot Arntz on 12/7/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import "CCHomeViewController.h"
+
+@interface CCHomeViewController ()
+
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *chatBarButtonItem;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *settingsBarButtonItem;
+@property (strong, nonatomic) IBOutlet UIImageView *photoImageView;
+@property (strong, nonatomic) IBOutlet UILabel *firstNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *ageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tagLineLabel;
+@property (strong, nonatomic) IBOutlet UIButton *likeButton;
+@property (strong, nonatomic) IBOutlet UIButton *infoButton;
+@property (strong, nonatomic) IBOutlet UIButton *dislikeButton;
+
+
+@end
+
+@implementation CCHomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - IBActions
+
+- (IBAction)likeButtonPressed:(UIButton *)sender
+{
+    
+}
+
+- (IBAction)dislikeButtonPressed:(UIButton *)sender
+{
+    
+}
+
+- (IBAction)infoButtonPressed:(UIButton *)sender
+{
+    
+}
+
+- (IBAction)settingsBarButtonItemPressed:(UIBarButtonItem *)sender
+{
+    
+}
+
+
+- (IBAction)chatBarButtonItemPressed:(UIBarButtonItem *)sender
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@end
